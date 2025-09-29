7d97f560b444ff89215ae208d0ec6528ec4ff0ea
KaMiL <kamil.esmund@gmail.com>
2025-09-29 10:11:33 +0200

    Home/CaseStudy-page

--- frontend-new/src/app/page.tsx
+++ frontend-new/src/app/page.tsx
@@ -4,8 +4,8 @@ import HeroSection from "./_components/HeroSection";
 import OfferSection from "./_components/OfferSection";
 import PromoSection from "./_components/PromoSection";
 import PartnersSection from "./_components/PartnersSection";
-// import CaseStudy from "./_components/CaseStudy";
-// import NewsSection from "./_components/NewsSection";
+import CaseStudy from "./_components/CaseStudy";
+import NewsSection from "./_components/NewsSection";
 // import ContactSection from "./_components/ContactSection";
 // import AboutSection from "./_components/AboutSection";
 
@@ -17,8 +17,8 @@ export default function HomePage() {
         <OfferSection/>
         <PromoSection />
         <PartnersSection />
-        {/* <CaseStudy /> */}
-        {/* <NewsSection /> */}
+        <CaseStudy />
+        <NewsSection />
         {/* <ContactSection /> */}
         {/* <AboutSection /> */}
     </main>
